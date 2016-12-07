de40e0a16fe3a6d0f23e882a541e4f65f8956ac3
Lorjax <Lorjax@gmx.de>
2016-12-07 14:35:28 +0100

    working POST, GET is now shipping out GeoJSON, added leaflet-page in root

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // Express
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
+
+var jsonParser = bodyParser.json();
 
 // Database
 var db = require('./queries');
@@ -10,17 +14,28 @@ var db = require('./queries');
 // Routes
 var router = express.Router();
 
-router.get('/', function(req, res) { });
-router.post('/', function(req, res) { });
+router.get('/', db.lookupPosts, function(req, res) {
+	res.json(req.post);
+});
+
+router.post('/', jsonParser, db.insertPost, function(req, res) {
+});
+
 router.get('/:id', db.lookupPost, function(req, res) {
 	res.json(req.post);
 });
+
 router.patch('/:id', function(req, res) { });
+
 router.delete('/:id', function(req, res) { });
 
 app.use('/api', router);
 
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname+'/index.html'));
+});
 
+// Listenport definieren
 app.listen(3000, function(){
 	console.log("Server running under port 3000.");
 })
--- queries.js
+++ queries.js
@@ -1,20 +1,24 @@
+// Dependencies
 var pg = require('pg');
+var geoJson = require('geojson');
 
+// Database configuration
 var connectionString = 'postgres://geoapp:geoapp@localhost:5432/geoapp';
-var connectionConfig = {user: 'geoapp', password: 'geoapp', database: 'geoapp', host: 'localhost', port: 5432}
+var client = new pg.Client(connectionString);
 
-var client = new pg.Client(connectionConfig);
-
-
+// Verbindung mit Datenbank herstellen
+client.connect(function (err) {
+	if (err) console.log("FEHLER bei Verbindungsaufbau mit Datenbank!\n" + err);
+});
 
 function lookupPost(req, res, next) {
+	// Id aus params des request holen
 	var postId = req.params.id;
+	console.log("[DEBUG] SELECT");
 	console.log("[DEBUG] Angefragte ID: " + postId);
-
+	// SQL-Statement mit Platzhalter $1 für id und Parsen zum Integer
 	var sql = 'SELECT * FROM Post WHERE id = $1::integer';
-	client.connect(function (err) {
-		if (err) console.log("FEHLER bei Verbindungsaufbau mit Datenbank!" + err);
-	});
+	// Ausführen des Query
 	query = client.query(sql, [ postId ], function(err, results) {
 		if(err) {
 			console.error(err);
@@ -26,18 +30,76 @@ function lookupPost(req, res, next) {
 			res.statusCode = 404;
 			return res.json({ errors: "No Post found"});
 		}
-		console.log(results.rows[0]);
-		req.post = results.rows[0];
+		req.post = geoJson.parse(results.rows[0], {Point: ['latitude', 'longitude']});
+		console.log("GeoJSON: \n" + JSON.stringify(req.post));
 		next();
 	});
 
-	// query = client.query("SELECT * FROM post WHERE id = $1", [ "1" ]);
+}
 
-	// query.on("row", function(row) {
-	// 	console.log(row)
-	// })
+function lookupPosts(req, res, next) {
+	// Id aus params des request holen
+	console.log("[DEBUG] SELECT ALL");
+	// SQL-Statement mit Platzhalter $1 für id und Parsen zum Integer
+	var sql = 'SELECT * FROM Post';
+	// Ausführen des Query
+	query = client.query(sql, function(err, results) {
+		if(err) {
+			console.error(err);
+			res.statusCode = 500;
+			return res.json({ errors: "could not retrieve posts" });
+		}
+
+		if(results.rows.length === 0) {
+			res.statusCode = 404;
+			return res.json({ errors: "No Posts found"});
+		}
+		var data = []
+		for (i = 0; i < results.rows.length; i++) {
+			data.push(results.rows[i]);
+		}
+		console.log(JSON.stringify(geoJson.parse(data, {Point: ['latitude', 'longitude']})));
+		//console.log(results.rows[0]);
+		req.post = geoJson.parse(data, {Point: ['latitude', 'longitude']});
+		//req.post += geoJson.parse(data, {Point: ['latitude', 'longitude']});
+		next();
+	});
+
+}
+
+function insertPost(req, res, next) {
+	console.log("[DEBUG] INSERT");
+	var sql = "INSERT INTO post(id, title, message, longitude, latitude) " +
+				"VALUES (DEFAULT, $1, $2, $3, $4) RETURNING id";
+	var data = [req.body.title, req.body.message, req.body.longitude, req.body.latitude];
+	console.log("[DEBUG] Insert-Parameter: \n");
+	console.log(data);
+	// Starte Insert
+	client.query(sql, data, function(err, result) {
+		if(err) {
+			console.error(err);
+			res.statusCode = 500;
+			return res.json({ errors: "could not create post" });
+		}
+
+		var sql = 'SELECT * FROM post WHERE id = $1';
+		client.query(sql, [result.rows[0].id], function(err, result) {
+			if(err) {
+				console.error(err);
+				res.statusCode = 500;
+				return res.json({ errors: "could not retrieve post after create" });
+			}
+		});
+
+		res.statusCode = 201;
+
+		res.json(result.rows[0]);
+
+	});	
 }
 
 module.exports = {
-	lookupPost: lookupPost
+	lookupPost: lookupPost,
+	insertPost: insertPost,
+	lookupPosts: lookupPosts
 };
